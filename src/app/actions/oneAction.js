0b2034907d565d0518dd71b4ec1750238b69d330
Himanshu Gupta <himanshugupta.396@gmail.com>
2017-12-12 17:26:19 +0530

    redux thunk(for async)  and promise-middleware(for async promises)

--- src/app/actions/oneAction.js
+++ src/app/actions/oneAction.js
@@ -1,10 +1,19 @@
 export function setUserId() {
-  return dispatch=>{
-    setTimeout(() =>{
-      dispatch({
-        type:'ADD',
-        payload:100
-      });
-    },2000);
-  }
+  // return dispatch=>{
+  //   setTimeout(() =>{
+  //     dispatch({
+  //       type:'ADD',
+  //       payload:100
+  //     });
+  //   },2000);
+  // }
+
+  return {
+    type:'ADD',
+    payload: new Promise((resolve,reject) =>{
+        setTimeout(() => {
+          resolve(10);
+        },2000)
+    })
+  };
 }
